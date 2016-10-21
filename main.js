7f4baac97f3611538c355a669ee47e6b416b9e10
Daniel Olasky <daniel.olasky@hotschedules.com>
2016-10-20 21:44:00 -0700

    initial commit

---
+++ main.js
@@ -0,0 +1,11 @@
+var data = require('./data.json');
+
+// 1) print the name of each person
+// 2) Print the name of the first 20 people
+// 3) Print the name and gender of every 3rd person
+// 4) Count how many people of each gender are in the collection
+// 5) Print the names of all the friends listed for each person in the collection
+// 6) Create a new collection of women over the age of 30
+// 7) Print the 2nd friend of every person with brown eyes
+//
+// you can run code by running `node main.js` in your terminal
